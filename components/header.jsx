8afa15bc87eafcaacc55539b59376d8e6cf084c1
chhotu1436 <kartikdhakad2020@gmail.com>
2025-11-07 17:40:06 +0530

    appomtment

--- components/header.jsx
+++ components/header.jsx
@@ -1,25 +1,25 @@
-import Link from 'next/link';
-import React from 'react';
+import React from "react";
+import { Button } from "./ui/button";
+import {
+  Calendar,
+  CreditCard,
+  ShieldCheck,
+  Stethoscope,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
+import { Badge } from "./ui/badge";
+import { checkAndAllocateCredits } from "@/actions/credits";
 import Image from "next/image";
 
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import { Button } from './ui/button';
-import { checkUser } from '@/lib/checkUser';
-import { Calendar, CreditCard, Stethoscope, User } from 'lucide-react';
-import { checkAndAllocateCredits } from '@/actions/credits';
-import { Badge } from './ui/badge';
-const Header = async () => {
+export default async function Header() {
   const user = await checkUser();
   if (user?.role === "PATIENT") {
-
     await checkAndAllocateCredits(user);
   }
+
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -32,6 +32,8 @@ const Header = async () => {
             className="h-10 w-auto object-contain"
           />
         </Link>
+
+        {/* Action Buttons */}
         <div className="flex items-center space-x-2">
           <SignedIn>
             {/* Admin Links */}
@@ -99,48 +101,51 @@ const Header = async () => {
             )}
           </SignedIn>
 
-          {(!user || user?.role === "PATIENT") && (
-            <Link href="/pricing">
+          {(!user || user?.role !== "ADMIN") && (
+            <Link href={user?.role === "PATIENT" ? "/pricing" : "/doctor"}>
               <Badge
                 variant="outline"
-                className="h-9 bg-emerald-900/20 border-emerald-700/30 px-3 py-1 flex items-centergap-2"
+                className="h-9 bg-emerald-900/20 border-emerald-700/30 px-3 py-1 flex items-center gap-2"
               >
-                <CreditCard className='h-3.5 w-3.5 text-emerald-400' />
-                <span className='text-emerald-400'>
-                  {user && user?.role === "PATIENT" ? (
+                <CreditCard className="h-3.5 w-3.5 text-emerald-400" />
+                <span className="text-emerald-400">
+                  {user && user.role !== "ADMIN" ? (
                     <>
                       {user.credits}{" "}
-                      <span className='hidden md:inline'>Credits</span>
+                      <span className="hidden md:inline">
+                        {user?.role === "PATIENT"
+                          ? "Credits"
+                          : "Earned Credits"}
+                      </span>
                     </>
-                  ) : (<>pricing</>)}
+                  ) : (
+                    <>Pricing</>
+                  )}
                 </span>
               </Badge>
             </Link>
           )}
 
-
           <SignedOut>
             <SignInButton>
-              <Button variant={"secondary"}>
-                Sign In
-              </Button>
+              <Button variant="secondary">Sign In</Button>
             </SignInButton>
           </SignedOut>
+
           <SignedIn>
-            <UserButton appearance={{
-              elements: {
-                avatarBox: "w-10 h-10",
-                userButtonPopoverCard: "shadow-xl",
-                userPreviewMainIdentifier: "font-semibold",
-              },
-            }}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
             />
           </SignedIn>
         </div>
       </nav>
-
     </header>
-  )
-};
-
-export default Header;
+  );
+}
